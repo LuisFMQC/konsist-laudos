249051ca626da1784a99c395ec0612b51326781b
Luis Fernando <luismqdf@gmail.com>
2023-07-28 16:38:01 -0300

    ajuste de efeito

--- src/Components/Content/Card.tsx
+++ src/Components/Content/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Card.module.css";
+import { transform } from "typescript";
 
 interface Card {
   aguardando: boolean | undefined;
@@ -20,10 +21,33 @@ const Card = ({
   tipo,
   link,
 }: Card) => {
+  const [isHovering, setIsHovering] = React.useState(false);
+  // Função de manipulador de evento para o onMouseOver
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  // Função de manipulador de evento para o onMouseOut
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
+  // Estilo baseado no estado de isHovering
+  const estiloDoElemento = {
+    background: aguardando ? "#ededed" : "#dedede",
+    cursor: aguardando ? "default" : "pointer",
+    transform: aguardando ? "none" : "",
+  };
+
   return (
     <>
       {aguardando ? (
-        <div className={styles.containerCard}>
+        <div
+          className={styles.containerCard}
+          style={estiloDoElemento}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
           <div className={styles.logoCard}>
             <img src={img} alt={`ícone do status do ${tipo}`} />
             {aguardando && <p>Aguardando Resultado</p>}
